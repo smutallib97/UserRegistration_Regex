30bef4c611801b6b9ecd1474ac577674b93316b2
smutallib97 <smutallib97@gmail.com>
2022-10-19 00:03:32 +0530

    Added: Ability to add Password - Rule3

--- UserRegistration.java
+++ UserRegistration.java
@@ -40,7 +40,7 @@ public class UserRegistration extends UserDetailsValidation {
         mobileNumber = scanner.next();
         UserDetailsValidation.isValidMobileNumber(mobileNumber);
     }
-    // UC5 - Ability to add Password - Rule2
+    // UC7 - Ability to add Password - Rule3
     public static void userPassword() {
         System.out.println("Enter your password");
         password = scanner.next();
